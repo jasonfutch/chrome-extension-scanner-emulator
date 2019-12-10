89600eae9b0070f116cdbec9edd6476ea7fd3e22
brandon <bsweet@envistacorp.com>
2019-12-10 16:39:04 -0500

    Brandons version of the dropdown menu

--- popup.js
+++ popup.js
@@ -13,18 +13,25 @@ document.addEventListener('DOMContentLoaded', function() {
   bgPort.postMessage({"action":"register"});
 
   $('#sendScan').click(function(){
-    keyPress('98798744',true)
+    keyPress('1H0478-R-K-34',true)
   });
 
+  // $('#sendScanUPC').click(function(){
+  //   keyPress('123456789999',true)
+  // });
   $('#sendScanUPC').click(function(){
-    keyPress('123456789999',true)
+    keyPress('073561001991',true)
   });
 
-  $('#sendSwipe').click(function() {
-    keyPress('%B1234123412341234^Carduser/John^030510100000019301000000877000000?;1234123412341234=0305101193010877?')
+  $('.sendSwipe').click(function() {
+    var val = $(this).attr('value');
+    keyPress(val);
+    
+    // keyPress('%B4111111111111111^Carduser/John^030510100000019301000000877000000?;4003000123456781=15125025432198712345?')
+    //keyPress('%B4111111111111111^Carduser/John^1911101100001100000000667000000?;4111111111111111=191110110000667?')
   });
 });
-
+<!--cardDetails.cardNumber = '4111111111111111'; cardDetails.month = '05'; cardDetails.year = '20'; cardDetails.csv = '123'; -->
 function keyPress(str,bolReturn){
   if(!bolReturn) bolReturn = false;
   bgPort.postMessage({"action":"keypress","value":str,"return":bolReturn});
